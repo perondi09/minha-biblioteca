9b29964e7c11eca0a74562462020d4326e18c51f
perondi <guilhermeperondi09@gmail.com>
2026-03-05 15:35:37 -0300

    feat: First commit

---
+++ src/test/java/com/perondi/minha_biblioteca/MinhaBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perondi.minha_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
